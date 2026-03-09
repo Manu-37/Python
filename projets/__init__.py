2b216c43c5e4a41eefa0ce65b85ba32ead3cc387
Manu-37 <emmanuel.despont.ind@gmail.com>
2026-03-09 12:09:03 +0100

    Maj Technique

---
+++ projets/__init__.py
@@ -0,0 +1,7 @@
+from .BaseRef_Manager.ui.BaseRef_UICore import BaseRef_UICore
+from .BaseRef_Manager.ui.views.Environment_FormView import Environment_FormView
+from .BaseRef_Manager.ui.views.Environment_ListView import Environment_ListView
+from .BaseRef_Manager.ui.views.Base_FormView import Base_FormView
+from .BaseRef_Manager.ui.views.Base_ListView import Base_ListView
+from .BaseRef_Manager.ui.views.Bas_Env_FormView import Bas_Env_FormView
+from .BaseRef_Manager.ui.views.Bas_Env_ListView import Bas_Env_ListView
