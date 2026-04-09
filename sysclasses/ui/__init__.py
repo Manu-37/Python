8104880b9ae6be37e2379f033e33ae4af0a5cbab
Manu-37 <emmanuel.despont.ind@gmail.com>
2026-04-10 01:11:01 +0200

    2 eme tour dans ui

--- sysclasses/ui/__init__.py
+++ sysclasses/ui/__init__.py
@@ -3,9 +3,12 @@ from .DataGrid import DataGrid
 from .AutoFormView import AutoFormView
 from .UI_Core import UI_Core
 from .Entity_ListView import Entity_ListView
-
-from .streamlit.clsStView        import clsStView
-from .streamlit.clsStChartView   import clsStChartView
-from .streamlit.clsStTableView   import clsStTableView
-from .streamlit.clsStFilterView  import clsStFilterView
+try:
+    from .streamlit.clsStView        import clsStView
+    from .streamlit.clsStChartView   import clsStChartView
+    from .streamlit.clsStTableView   import clsStTableView
+    from .streamlit.clsStFilterView  import clsStFilterView
+except ImportError:
+    # Si Streamlit n'est pas installé, on ignore les classes spécifiques à Streamlit
+    pass
  
